f5246734b6a87baada87297ce27379c631762972
Md. Abir mahmud <abirmahmud5665@gmail.com>
2024-11-26 19:00:53 +0600

    done : remove unnecessary files and create necessary folders

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      h2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quo,
+      hic, quod non praesentium soluta debitis iure rem perferendis libero
+      corporis! Sunt quis omnis pariatur aliquam, magnam quasi maiores labore.
+    </div>
+  );
+};
+
+export default page;
